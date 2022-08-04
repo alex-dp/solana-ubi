98000883ef4de6c39a57f525e2839d4f8bf52e45
Alessandro De Pasquale <depasquale.a@tuta.io>
2022-08-04 20:25:02 +0200

    final logic

--- programs/solana-ubi/src/lib.rs
+++ programs/solana-ubi/src/lib.rs
@@ -1,8 +1,10 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::clock::UnixTimestamp;
 use anchor_lang::solana_program::program_option::COption;
 use anchor_spl::token::{self, Mint, MintTo, TokenAccount};
 
-const MINTER:&str = "minter";
+const MINTER: &str = "minter";
+const TRUST_COEFF: u8 = 0;
 
 declare_id!("EcFTDXxknt3vRBi1pVZYN7SjZLcbHjJRAmCmjZ7Js3fd");
 
@@ -16,23 +18,65 @@ pub mod ido_pool {
 
     pub fn mint_token(
         ctx: Context<MintUBI>
-    ) -> Result<()> {
-        // Mint Redeemable to user Redeemable account.
-        let seeds = &[
-            MINTER.as_bytes(),
-            //&[BUMP]
-        ];
-        let signer = &[&seeds[..]];
-        let cpi_accounts = MintTo {
-            mint: ctx.accounts.redeemable_mint.to_account_info(),
-            to: ctx.accounts.user_redeemable.to_account_info(),
-            authority: ctx.accounts.pool_signer.clone(),
-        };
-        let cpi_program = ctx.accounts.token_program.clone();
-        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
-        token::mint_to(cpi_ctx, 10_000_000_000)?;
+    ) -> Result<u8> {
+        let now_ts = Clock::get().unwrap().unix_timestamp;
+        if !ctx.accounts.ubi_info.trusted {
+            Err(0)
+        } else if now_ts < ctx.accounts.ubi_info.last_issuance + 23 * 60 * 60 {
+            Err(1)
+        } else {
+            // approx rate of 10 tok per day (9 decimal places)
+            let amount: u64 = (115_740 * (now_ts - ctx.accounts.ubi_info.last_issuance)) as u64;
+            // Mint Redeemable to user Redeemable account.
+            let seeds = &[
+                MINTER.as_bytes(),
+                &[255]//&[BUMP]
+            ];
+            let signer = &[&seeds[..]];
+            let cpi_accounts = MintTo {
+                mint: ctx.accounts.redeemable_mint.to_account_info(),
+                to: ctx.accounts.user_redeemable.to_account_info(),
+                authority: ctx.accounts.pool_signer.clone(),
+            };
+            let cpi_program = ctx.accounts.token_program.clone();
+            let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
+            token::mint_to(cpi_ctx, amount)?;
+            ctx.accounts.ubi_info.last_issuance = now_ts;
+            Ok(Ok(0))
+        }.expect("")
+    }
 
-        Ok(())
+    // TODO user can't trust themselves
+    pub fn trust(ctx: Context<TrustUser>) -> Result<u8> {
+        let now_ts: UnixTimestamp = Clock::get().unwrap().unix_timestamp;
+        if ctx.accounts.trustee_ubi_info.trusted {
+            Err(0)
+        } else if ctx.accounts.trustee_ubi_info.trusters.contains(ctx.accounts.truster_authority.key) {
+            Err(1)
+        } else if now_ts < ctx.accounts.truster_ubi_info.last_trust_given + 24 * 60 * 60 {
+            Err(2)
+        } else {
+            ctx.accounts.trustee_ubi_info.trusters.push(*ctx.accounts.truster_authority.key);
+
+            if ctx.accounts.trustee_ubi_info.trusters.len() >= TRUST_COEFF as usize {
+                ctx.accounts.trustee_ubi_info.trusted = true;
+            }
+            ctx.accounts.truster_ubi_info.last_trust_given = now_ts;
+            Ok(Ok(0))
+        }.expect("")
+    }
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<u8> {
+        let now_ts: UnixTimestamp = Clock::get().unwrap().unix_timestamp;
+        let acc = &mut ctx.accounts.ubi_info;
+
+        acc.trusters = Vec::new();
+        acc.last_trust_given = now_ts - 24 * 60 * 60;
+        acc.trusted = true; //TODO change to false for production
+        acc.last_issuance = now_ts;
+        acc.bump = 255; //TODO check this
+
+        Ok(0)
     }
 }
 
@@ -41,14 +85,58 @@ pub struct MintUBI<'info> {
     /// CHECK: x
     #[account(seeds = [MINTER.as_bytes()], bump)]
     pool_signer: AccountInfo<'info>,
-    #[account(mut, constraint = redeemable_mint.mint_authority == COption::Some(*pool_signer.key))]
+    #[account(mut, constraint = redeemable_mint.mint_authority == COption::Some(* pool_signer.key))]
     pub redeemable_mint: Account<'info, Mint>,
     /// CHECK: x
-    #[account(signer)]
+    #[account(signer, mut)]
     pub user_authority: AccountInfo<'info>,
     #[account(mut)]
     pub user_redeemable: Account<'info, TokenAccount>,
+    // is program account. TODO SET SEEDS!
+    #[account(mut, seeds = ["ubi_info".as_bytes()], bump)]
+    pub ubi_info: Account<'info, UBIInfo>,
     /// CHECK: x
-    #[account(constraint = token_program.key == &token::ID)]
-    pub token_program: AccountInfo<'info>
+    #[account(constraint = token_program.key == & token::ID)]
+    pub token_program: AccountInfo<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct TrustUser<'info> {
+    #[account(mut)]
+    pub trustee_ubi_info: Account<'info, UBIInfo>,
+    #[account(mut)]
+    pub truster_ubi_info: Account<'info, UBIInfo>,
+    /// CHECK: x
+    #[account(signer, mut)]
+    pub truster_authority: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user_authority, space = 8 + UBIInfo::MAX_SIZE, seeds = ["ubi_info".as_bytes()], bump)]
+    pub ubi_info: Account<'info, UBIInfo>,
+    /// CHECK: x
+    #[account(signer, mut)]
+    pub user_authority: AccountInfo<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct UBIInfo {
+    // i64
+    last_issuance: UnixTimestamp,
+    // i64
+    last_trust_given: UnixTimestamp,
+    // [u8; 32] * TRUST_COEFF
+    trusters: Vec<Pubkey>,
+    trusted: bool,
+    bump: u8,
+}
+
+impl UBIInfo {
+    // in bytes
+    pub const MAX_SIZE: usize = 8 + 8 + (4 + 32 * TRUST_COEFF as usize) + 1 + 1;
 }
--- send.py
+++ send.py
@@ -4,12 +4,19 @@ from solana.transaction import AccountMeta
 from solana.rpc.api import Client
 from solana.transaction import Transaction, TransactionInstruction
 from spl.token.constants import TOKEN_PROGRAM_ID
+from solana.system_program import SYS_PROGRAM_ID
 
 client = Client("http://localhost:8899")
 
 sender = Keypair.from_seed(bytes(PublicKey(1)))
 
 print(sender.public_key)
+
+prog_id = PublicKey("EcFTDXxknt3vRBi1pVZYN7SjZLcbHjJRAmCmjZ7Js3fd")
+
+ubi_info, bump = PublicKey.find_program_address([b"ubi_info"], prog_id)
+
+print(ubi_info)
 
 txn = Transaction().add(
 	TransactionInstruction([
@@ -21,11 +28,15 @@ txn = Transaction().add(
 		AccountMeta(sender.public_key, True, False),
 		# token acc
 		AccountMeta(PublicKey("HfNY5k4T4xQVeYASUvDZE12MRyCj4hqGNJ6yuZGPshAx"), False, True),
+		# ubi info
+		AccountMeta(ubi_info, False, True),
 		# token prog
 		AccountMeta(TOKEN_PROGRAM_ID, False, False),
+		# system prog
+		AccountMeta(SYS_PROGRAM_ID, False, False),
 		],
 	# program address
-	PublicKey("EcFTDXxknt3vRBi1pVZYN7SjZLcbHjJRAmCmjZ7Js3fd"),
+	prog_id,
 	# data
 	# instruction offset
 	bytes(b"\xac\x89\xb7\x0e\xcfn\xea8")
@@ -34,5 +45,4 @@ txn = Transaction().add(
 
 txn.fee_payer = sender.public_key
 
- 
 client.send_transaction(txn, sender)
